20212910f363a1ba3946ea9f69285e5596d7eb53
shreyas00567 <shreyas.khanore24@vit.edu>
2026-03-29 03:26:26 +0000

    reimbursement module structure

---
+++ reimbursement_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+'name':'Reimbursement Management',
+
+'version':'1.0',
+
+'depends':['base','hr'],
+
+'data':[
+
+'security/ir.model.access.csv'
+
+],
+
+'installable':True,
+
+'application':True
+}
