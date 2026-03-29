9940fc8c2ec01f8014cadae93c4038d0f727c53e
shreyas00567 <shreyas.khanore24@vit.edu>
2026-03-29 15:40:03 +0530

    Update __manifest__.py

--- reimbursement_management/__manifest__.py
+++ reimbursement_management/__manifest__.py
@@ -15,3 +15,25 @@
 
 'application':True
 }
+{
+    'name':'Reimbursement Management',
+
+    'version':'1.0',
+
+    'summary':'Expense reimbursement workflow management',
+
+    'depends':[
+        'base',
+        'hr'
+    ],
+
+    'data':[
+
+        'security/ir.model.access.csv'
+
+    ],
+
+    'installable':True,
+
+    'application':True
+}
